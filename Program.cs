536d45a1402b894c2fda1c7b2930ba4793bcaee4
hyojung <rlagywnd05@gmail.com>
2020-09-12 16:21:30 +0900

    star3

--- Program.cs
+++ Program.cs
@@ -40,10 +40,38 @@ namespace STAR3
                 Console.WriteLine("");
                 m--;
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
            
+            for (int i = 1; i < 11; i++)
+            {
+                for (int k = 0; k < 11-i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j =0; j<2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for (int i = 9; i > 0; i--)
+            {
+                for (int j = 0; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+               
+                Console.WriteLine("");
+            }
             Console.ReadLine();
-
-
         }
     }
 }
