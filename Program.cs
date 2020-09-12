d95171f8b59ee0d31efa9f1f57a08b726c296555
hyojung <rlagywnd05@gmail.com>
2020-09-12 15:25:25 +0900

    star1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace STAR3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 9;
+            for(int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < m; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+                m--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
